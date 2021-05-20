ccad28accf199bd8c0465125791b42bd5a47a056
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-05-20 13:37:09 -0300

    update home

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,10 @@ export default class MyDocument extends Document {
     return(
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&family=Red+Rose:wght@500&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+          <title>Shark Dashboard</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&family=Red+Rose:wght@500&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 import styles from '../styles/index.module.scss';
 
 export default function Home() {
   const router = useRouter();
+
+  useEffect(() => {
+    router.push('/dashboard/home');
+  }, []);
 
   return (
     <main className={styles.main}>
