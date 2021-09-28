6b6db8e32a5b2eaec49bc87d996787e0c419e9cd
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-09-28 16:18:02 -0300

    firebase config

--- sharkdashboard/src/services/firebase.ts
+++ sharkdashboard/src/services/firebase.ts
@@ -1,13 +1,21 @@
-import firebase from 'firebase/app'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import 'firebase/auth'
-// import 'firebase/database'
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
-}
-
-firebase.initializeApp(firebaseConfig)
-
-// export const auth = firebase.auth()
-// export const database = firebase.database()
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
