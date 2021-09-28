5eaf2646be9c54ee6f566680282ecc7ab5c4de21
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-09-28 14:28:04 -0300

    reset project

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/globals.scss';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-      <Component {...pageProps} />
-  )
-}
--- src/pages/login/home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home(){
-  return(
-    <div>
-      <h1>LOGIN</h1>
-    </div>
-  )
-}
